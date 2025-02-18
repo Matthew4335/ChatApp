f73bb80381521fb4b260553a9f2d8f19548e1002
Matthew <mbrown411@gatech.edu>
2025-02-17 23:03:34 -0600

    Login controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -5,6 +5,9 @@ import bcrypt from "bcryptjs"
 export const signup = async (req, res) => {
     const {fullName, email, password} = req.body;
     try {
+        if (!fullName || !email || !password) {
+            return res.status(400).json({ message: "All fields are required"});
+        }
         if (password.length < 6) {
             return res.status(400).json({ message: "Password must be at least 6 characters"});
         }
@@ -33,12 +36,37 @@ export const signup = async (req, res) => {
         }
     } catch (error) {
         console.log("Error in signup controller");
-        res.status(500).json({message: "Internal Service Error"})
+        res.status(500).json({ message: "Internal Service Error", error: error.message });
     }
 };
 
-export const login = (req, res) => {
-    res.send("login route")
+export const login = async (req, res) => {
+    const { email, password} = req.body;
+    try {
+        const user = await User.findOne({email});
+
+        if (!user) {
+            return res.status(400).json({message:"Invalid email or password"});
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(400).json({message:"Invalid email or password"});
+        }
+
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id:user._id,
+            fullName:user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+
+    } catch {
+        console.log("Error in login controller");
+        res.status(500).json({ message:"Internal Service Error", error: error.message});
+    }
 };
 
 export const logout = (req, res) => {
